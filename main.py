1ad2db2431c3348df84ce1a415723827e2872b79
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-23 21:13:03 +0300

    import statements are added to main

---
+++ main.py
@@ -0,0 +1,7 @@
+from binance.client import Client
+from binanceAPI.position_utilities import enter_long, enter_short
+from config import api_key, secret_key
+from data.io_utilities import print_with_color, calculateWR, print_position_message
+from time import sleep
+from data.data_functions import save_position, save_result
+import copy
